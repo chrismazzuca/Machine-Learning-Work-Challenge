6101a3b77fe7f63bce3f33d59d0a91ad9c623c50
chrismazzuca <chrismazz-7@live.com>
2020-04-01 19:30:11 -0400

    Visualize employment data in each province (by age group and sex)

--- visualize_data.py
+++ visualize_data.py
@@ -30,11 +30,46 @@ def savePieChart(data, file, filePath):
     fig.figure.savefig(filePath)
 
 
+# Create and save a bar chart
+def saveBarChart(data, file, filePath):
+    # Get the participation, employment, and unemployment rates from the data
+    participation_rate = data.loc['Participation rate', '19-Feb'].values
+    employment_rate = data.loc['Employment rate', '19-Feb'].values
+    unemployment_rate = data.loc['Unemployment rate', '19-Feb'].values
+
+    # Plot a bar chart of the data
+    labels = ['Both (15+)', 'Both (15-24)', 'Men (25+)', 'Women (25+)']
+    x = np.arange(len(labels))
+    fig, ax = plt.subplots()
+    ax.bar(x - 0.25, participation_rate, 0.25, label='Participation Rate')
+    ax.bar(x, employment_rate, 0.25, label='Employment Rate')
+    ax.bar(x + 0.25, unemployment_rate, 0.25, label='Unemployment Rate')
+
+    # Add labels and save the chart
+    ax.set_ylabel('Percentage')
+    ax.set_title('Employment rates ({})'.format(file[:-4]))
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.legend()
+    plt.show()
+    ax.figure.savefig(filePath)
+
+
 ############## Visualize Employment Data by industry ##############
-subdir = "Employment Data/By industry/"
-for _, _, files in os.walk(subdir):
-    for file in files:
-        # Read each file, and create a pie chart
-        data = pd.read_csv(subdir + file, index_col=0)
-        savePieChart(data, file, 'Plots/By industry/' + file[:-4])
+# subdir = "Employment Data/By industry/"
+# for _, _, files in os.walk(subdir):
+#     for file in files:
+#         # Read each file, and create a pie chart
+#         data = pd.read_csv(subdir + file, index_col=0)
+#         savePieChart(data, file, 'Plots/By industry/' + file[:-4])
+###################################################################
+
+
+########## Visualize Employment Data by age group and sex ##########
+# subdir = "Employment Data/By age group and sex/"
+# for _, _, files in os.walk(subdir):
+#     for file in files:
+#         # Read each file, and create a pie chart
+#         data = pd.read_csv(subdir + file, index_col=0)
+#         saveBarChart(data, file, 'Plots/By age group and sex/' + file[:-4])
 ###################################################################
